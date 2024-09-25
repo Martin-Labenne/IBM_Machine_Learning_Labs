97b78700313b2242077790f7b79edc9ed818254d
Martin-Labenne <martinlabenne@gmail.com>
2024-09-25 15:16:33 +0200

    Multiple regression lab

---
+++ constants.py
@@ -0,0 +1,2 @@
+DATA_FOLDER = './data/'
+FUEL_CONSUMPTION_DATA_URL = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
