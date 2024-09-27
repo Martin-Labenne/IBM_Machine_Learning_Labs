ce5cf457be4fb1e8e40395568816757b3d9a593e
Martin-Labenne <martinlabenne@gmail.com>
2024-09-27 17:38:46 +0200

    complete kNN Lab

--- constants.py
+++ constants.py
@@ -1,2 +1,3 @@
 DATA_FOLDER = './data/'
-FUEL_CONSUMPTION_DATA_URL = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
+FUEL_CONSUMPTION_DATA_URL = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv'
+CUST_CAT_DATA_URL = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv'
