b5af795e6ec646e63f9a99938fa2e729e792c68e
Martin-Labenne <martinlabenne@gmail.com>
2024-09-30 16:52:21 +0200

    decision tree classification lab

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 DATA_FOLDER = './data/'
 FUEL_CONSUMPTION_DATA_URL = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv'
 CUST_CAT_DATA_URL = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv'
+DRUG_DATA_URL = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/drug200.csv'
