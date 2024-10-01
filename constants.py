fe7173c3821ba09ab26a4bfa28305035ac359e5e
Martin-Labenne <martinlabenne@gmail.com>
2024-10-01 13:22:49 +0200

    regression tree v2 lab

--- constants.py
+++ constants.py
@@ -4,4 +4,4 @@ CUST_CAT_DATA_URL = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomai
 DRUG_DATA_URL = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/drug200.csv'
 CREDIT_CARD_DATA_URL = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/creditcard.csv"
 CHURN_DATA_URL = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv"
-
+TAXI_DATA_URL = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/yellow_tripdata_2019-06.csv"
